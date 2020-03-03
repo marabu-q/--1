93cf3a2b406dd7f3d54f878382dd49319123248f
Albert <salyakh0v@yandex.ru>
2020-03-03 23:20:57 +0300

    6th commit

--- ex1js.js
+++ ex1js.js
@@ -1,5 +1,13 @@
-let money=+prompt("СКОЛЬКА ТЫ ЗАРАБАТЫВАЕШЬ?",""),
+let money, time;
+function start() {
+    money=+prompt("СКОЛЬКА ТЫ ЗАРАБАТЫВАЕШЬ?","");
     data=prompt("Введите время и дату в формате DD.MM.YYYY!", "");
+    }
+start();
+    
+
+
+
 
 let AppData = {
            budget: money,
@@ -7,18 +15,67 @@ let AppData = {
            expenses: {},
            optionalExpenses: {},
            income: [],
-           savings: false,
+           savings: true,
 };
 
-for (let i = 0; i < 2; i++) {
+
+function chooseExpenses() {
+    for (let i = 0; i < 2; i++) {
         let a=prompt("Обязательная статья расходов"),
             b=prompt("Во сколько обойдется?");
          if (typeof(a)==='string' && typeof(a)!=null && typeof(b)!=null && a!= '' && b!= '') {
                console.log("done");
                AppData.expenses[a] = b;
             
-            }
+         } else {
+             i = i - 1;
+         }
+    }
+}
+chooseExpenses();
+
+
+
+
+function detectDayBudget() {
+    AppData.moneyPerDay=(AppData.budget / 30).toFixed();
+    alert(AppData.moneyPerDay);
 }
 
+detectDayBudget();
 
-alert(money/30);
+function detectLevel() {
+    if (AppData.moneyPerDay<500){
+        alert("Ежедневный бюджет минимальный");
+    } else if(AppData.moneyPerDay<1000){
+        alert("Ежедневный бюджет низкий");
+    } else if(AppData.moneyPerDay<3000) {
+        alert("Ежедневный бюджет средний");
+    } else {
+        alert("Ежедневный бюджет высокий");
+    }
+}
+
+detectLevel();
+
+function checkSavings() {
+      if (AppData.savings==true) {
+          let save=+prompt("Сумма накоплений"),
+              percent=+prompt("Процент");
+        
+        AppData.monthIncome=save/100/12*percent;
+        alert(AppData.monthIncome);
+      }
+}
+
+checkSavings();
+
+let answer1, answer2, answer3;
+
+function choseOptionalExpenses() {
+     answer1=pormpt("Введите статью необязательных расходов");
+     answer2=pormpt("Введите статью необязательных расходов");
+     answer3=pormpt("Введите статью необязательных расходов");
+}
+
+choseOptionalExpenses();
